c7ed1ea2f5f38c88544319933c4b9d59297903fa
Takeyuki Moriyama <3154232+t-m0riyama@users.noreply.github.com>
2024-06-14 14:36:00 +0900

    fix Teamsメッセージ送信時の遅延を環境変数でセットする際に、キャストされていなかった問題を修正

--- awx_demo/notification/notificator.py
+++ awx_demo/notification/notificator.py
@@ -15,7 +15,7 @@ class Notificator:
         match notification_spec.notification_type:
             case Notificator.TEAMS_NOTIFICATION:
                 TeamsNotificator.notify(notification_spec)
-                teams_webhook_message_delay_msec = os.getenv("TEAMS_WEB_HOOK_MESSAGE_DELAY_MSEC", 500)
+                teams_webhook_message_delay_msec = int(os.getenv("TEAMS_WEB_HOOK_MESSAGE_DELAY_MSEC", 500))
                 time.sleep(teams_webhook_message_delay_msec * 0.001)
             # case Notificator.MAIL_NOTIFICATION:
             #     MailNotificator.notify(notification_spec)
