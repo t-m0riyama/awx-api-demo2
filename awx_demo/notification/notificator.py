857e381a05318270458a8728ee3c2d6d69e63a24
Takeyuki Moriyama <3154232+t-m0riyama@users.noreply.github.com>
2024-06-14 14:21:47 +0900

    change Teams通知を行う際に遅延時間を指定できる様に変更（デフォルト: 500ms）

--- awx_demo/notification/notificator.py
+++ awx_demo/notification/notificator.py
@@ -1,4 +1,5 @@
-from enum import IntEnum, auto
+import os
+import time
 
 from awx_demo.notification.notification_spec import NotificationSpec
 from awx_demo.notification.teams_notificator import TeamsNotificator
@@ -14,5 +15,7 @@ class Notificator:
         match notification_spec.notification_type:
             case Notificator.TEAMS_NOTIFICATION:
                 TeamsNotificator.notify(notification_spec)
+                teams_webhook_message_delay_msec = os.getenv("TEAMS_WEB_HOOK_URL", 500)
+                time.sleep(teams_webhook_message_delay_msec * 0.001)
             # case Notificator.MAIL_NOTIFICATION:
             #     MailNotificator.notify(notification_spec)
