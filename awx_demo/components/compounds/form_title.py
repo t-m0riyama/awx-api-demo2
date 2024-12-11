6b527cbb39a50d03aae962f5e33f14098190f9b6
Takeyuki Moriyama <3154232+t-m0riyama@users.noreply.github.com>
2024-12-11 09:16:33 +0900

    add フォームタイトルのフォントサイズを指定できるパラメータを追加

--- awx_demo/components/compounds/form_title.py
+++ awx_demo/components/compounds/form_title.py
@@ -2,10 +2,15 @@ import flet as ft
 
 
 class FormTitle(ft.Column):
+    # const
+    TITLE_FONT_SIZES_LARGE = (36, 30)
+    TITLE_FONT_SIZES_MEDIUM = (30, 24)
+    TITLE_FONT_SIZES_SMALL = (24, 18)
 
-    def __init__(self, title, sub_title):
+    def __init__(self, title, sub_title, title_font_sizes=TITLE_FONT_SIZES_MEDIUM):
         self.title = title
         self.sub_title = sub_title
+        self.title_font_sizes = title_font_sizes
         controls = None
         if self.sub_title:
             controls = [
@@ -15,7 +20,7 @@ class FormTitle(ft.Column):
                             ft.Text(
                                 value=self.title,
                                 # theme_style=ft.TextThemeStyle.DISPLAY_MEDIUM,
-                                size=30,
+                                size=self.title_font_sizes[0],
                                 weight=ft.FontWeight.BOLD,
                                 color=ft.colors.ON_SECONDARY,
                                 text_align=ft.TextAlign.CENTER
@@ -31,7 +36,7 @@ class FormTitle(ft.Column):
                             ft.Text(
                                 value=self.sub_title,
                                 # theme_style=ft.TextThemeStyle.DISPLAY_SMALL,
-                                size=24,
+                                size=self.title_font_sizes[1],
                                 weight=ft.FontWeight.NORMAL,
                                 text_align=ft.TextAlign.CENTER
                             ),
@@ -48,7 +53,7 @@ class FormTitle(ft.Column):
                             ft.Text(
                                 value=self.title,
                                 # theme_style=ft.TextThemeStyle.DISPLAY_MEDIUM,
-                                size=30,
+                                size=self.title_font_sizes[0],
                                 weight=ft.FontWeight.BOLD,
                                 color=ft.colors.ON_SECONDARY,
                                 text_align=ft.TextAlign.CENTER,
