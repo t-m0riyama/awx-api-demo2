8aa6ac0ac335f6f9bba811ec5d851bfbd29fff5b
Takeyuki Moriyama <3154232+t-m0riyama@users.noreply.github.com>
2024-12-11 09:33:00 +0900

    add 簡易ヘルプの追加

--- awx_demo/components/app_header.py
+++ awx_demo/components/app_header.py
@@ -1,5 +1,6 @@
 import flet as ft
 
+from awx_demo.components.forms.context_help_form import ContextHelpForm
 from awx_demo.components.keyboard_shortcut_manager import KeyboardShortcutManager
 from awx_demo.components.session_helper import SessionHelper
 from awx_demo.components.types.user_role import UserRole
@@ -16,6 +17,34 @@ class AppHeader(ft.Row):
         self.session = session
         self.page = page
         self.app_title = app_title
+        content_md = f"""
+定型的な設定変更・作業を依頼するために申請を行い、作業担当者は申請内容に基づいて実行することができます。
+***
+
+### :white_check_mark: 申請を作成するには
+* 「新規作成」ボタンをクリックし、申請を作成します。
+
+### :white_check_mark: 申請内容を変更するには
+* 申請一覧から、変更を行いたい申請の「依頼ID」をクリックし、申請の内容を変更できます。
+
+### :white_check_mark: 申請内容を実行するには
+* 申請一覧から、実行を行いたい申請の「依頼ID」をクリックし、申請の内容を実行できます。
+        """
+
+        formContextHelp = ContextHelpForm(self.session, self.page, title=f"{self.page.session.get('app_title_base')} へようこそ", content_md=content_md)
+        self.dlgContextForm = ft.AlertDialog(
+            modal=True,
+            content=formContextHelp,
+            actions=[
+                ft.FilledButton("OK", tooltip="OK (Control+Shift+Y)", on_click=self.on_click_context_help_ok),
+            ],
+            actions_alignment=ft.MainAxisAlignment.END,
+        )
+        self.help_icon = ft.IconButton(
+            icon=ft.icons.HELP_OUTLINE_ROUNDED,
+            tooltip="ヘルプ",
+            on_click=self.on_click_context_help,
+        )
         self.toggle_dark_light_icon = ft.IconButton(
             icon="light_mode",
             selected_icon="dark_mode",
@@ -53,6 +82,7 @@ class AppHeader(ft.Row):
                 ft.Container(
                     content=ft.Row(
                         [
+                            self.help_icon,
                             self.toggle_dark_light_icon,
                             ft.PopupMenuButton(
                                 icon=ft.icons.ACCOUNT_CIRCLE,
@@ -107,6 +137,22 @@ class AppHeader(ft.Row):
         self.page.update()
 
     @Logging.func_logger
+    def on_click_context_help(self, e):
+        self._lock_form_controls()
+        self._save_keyboard_shortcuts()
+        self.page.open(self.dlgContextForm)
+        self.dlgContextForm.open = True
+        self._unlock_form_controls()
+        self.page.update()
+
+    @Logging.func_logger
+    def on_click_context_help_ok(self, e):
+        self.page.close(self.dlgContextForm)
+        self.dlgContextForm.open = False
+        self._restore_key_shortcuts()
+        self.page.update()
+
+    @Logging.func_logger
     def logout_clicked(self, e):
         activity_spec = ActivityHelper.ActivitySpec(
             user=self.session.get("awx_loginid"),
@@ -122,3 +168,26 @@ class AppHeader(ft.Row):
         )
         SessionHelper.clean_session(self.session)
         self.page.go("/login")
+
+    @Logging.func_logger
+    def _lock_form_controls(self):
+        # クリック連打対策
+        self.page.appbar.disabled = True
+        self.page.appbar.update()
+
+    @Logging.func_logger
+    def _unlock_form_controls(self):
+        # クリック連打対策解除
+        self.page.appbar.disabled = False
+        self.page.appbar.update()
+
+    @Logging.func_logger
+    def _save_keyboard_shortcuts(self):
+        keybord_shortcut_manager = KeyboardShortcutManager(self.page)
+        keybord_shortcut_manager.save_key_shortcuts()
+        keybord_shortcut_manager.clear_key_shortcuts()
+
+    @Logging.func_logger
+    def _restore_key_shortcuts(self):
+        keybord_shortcut_manager = KeyboardShortcutManager(self.page)
+        keybord_shortcut_manager.restore_key_shortcuts()
---
+++ awx_demo/components/forms/context_help_form.py
@@ -0,0 +1,52 @@
+import flet as ft
+
+from awx_demo.components.compounds.form_description import FormDescription
+from awx_demo.components.compounds.form_title import FormTitle
+
+
+class ContextHelpForm(ft.Card):
+    """コンテキストヘルプ フォーム"""
+
+    # const
+    CONTENT_HEIGHT = 500
+    CONTENT_WIDTH = 480
+    BODY_HEIGHT = 350
+
+    def __init__(self, session, page: ft.Page, title: str, content_md: str):
+        self.session = session
+        self.page = page
+        self.title = title
+        help_content_md = ft.Markdown(
+            content_md,
+            selectable=True,
+            extension_set=ft.MarkdownExtensionSet.GITHUB_WEB,
+            on_tap_link=lambda e: page.launch_url(e.data),
+        )
+
+        # controls
+        formTitle = FormTitle(f'ヘルプ :    {self.title}', None, title_font_sizes=FormTitle.TITLE_FONT_SIZES_SMALL)
+        header = ft.Container(
+            formTitle,
+            margin=ft.margin.only(bottom=20),
+        )
+        body = ft.Column(
+            [
+                help_content_md,
+            ],
+            height=self.BODY_HEIGHT,
+        )
+
+        controls = ft.Container(
+            ft.Column(
+                [
+                    header,
+                    body,
+                ],
+                alignment=ft.MainAxisAlignment.CENTER,
+            ),
+            width=self.CONTENT_WIDTH,
+            height=self.CONTENT_HEIGHT,
+            padding=30,
+        )
+
+        super().__init__(controls)
