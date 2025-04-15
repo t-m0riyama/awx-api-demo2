60cd967e42e2e61dc46379737dc07865ac5837d9
Takeyuki Moriyama <3154232+t-m0riyama@users.noreply.github.com>
2025-04-15 16:41:06 +0900

    change 簡易ヘルプ画面の横幅を調整

--- awx_demo/components/forms/context_help_form.py
+++ awx_demo/components/forms/context_help_form.py
@@ -9,7 +9,7 @@ class ContextHelpForm(ft.Card):
 
     # const
     CONTENT_HEIGHT = 500
-    CONTENT_WIDTH = 500
+    CONTENT_WIDTH = 520
     BODY_HEIGHT = 350
 
     def __init__(self, session, page: ft.Page, title: str, content_md: str):
