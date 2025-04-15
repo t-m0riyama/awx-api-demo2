bcc4eae6b0dbb0e488f4d98f4927af358a53bae4
Takeyuki Moriyama <3154232+t-m0riyama@users.noreply.github.com>
2025-04-15 16:43:24 +0900

    change ダッシュボードフォームの不要なコードを削除

--- awx_demo/components/forms/dashboard_form.py
+++ awx_demo/components/forms/dashboard_form.py
@@ -1,16 +1,8 @@
-import os
-
 import flet as ft
 
-from awx_demo.awx_api.awx_api_helper import AWXApiHelper
 from awx_demo.components.compounds.form_title import FormTitle
-from awx_demo.components.compounds.parameter_input_text import ParameterInputText
 from awx_demo.components.forms.dashboard_tab.in_progress_tab_form import InProgressTabForm
-from awx_demo.components.forms.helper.activity_row_helper import ActivityRowData, ActivityRowHelper
 from awx_demo.components.keyboard_shortcut_manager import KeyboardShortcutManager
-from awx_demo.components.session_helper import SessionHelper
-from awx_demo.components.types.user_role import UserRole
-from awx_demo.utils.event_helper import EventType
 from awx_demo.utils.logging import Logging
 
 
@@ -87,9 +79,6 @@ class DashboardForm(ft.Card):
                     ),
                 ]
             ),
-            # width=self.CONTENT_WIDTH,
-            # height=self.CONTENT_HEIGHT,
-            # padding=ft.padding.all(0),
         )
         self.register_key_shortcuts()
         super().__init__(content=self.controls, expand=True)
@@ -102,35 +91,7 @@ class DashboardForm(ft.Card):
             key_set=keyboard_shortcut_manager.create_key_set(
                 key="F", shift=True, ctrl=False, alt=True, meta=False
             ),
-            func=lambda e: self.dropUser.focus()
-        )
-        # 概要に含まれる文字の検索を実行
-        keyboard_shortcut_manager.register_key_shortcut(
-            key_set=keyboard_shortcut_manager.create_key_set(
-                key="Enter", shift=False, ctrl=True, alt=False, meta=False
-            ),
-            func=self.on_click_search_summary,
-        )
-        # 操作履歴一覧の再読み込み
-        keyboard_shortcut_manager.register_key_shortcut(
-            key_set=keyboard_shortcut_manager.create_key_set(
-                key="R", shift=False, ctrl=True, alt=True, meta=False,
-            ),
-            func=lambda e: self.refresh(),
-        )
-        # 操作履歴一覧のページ送り / 次のページへ
-        keyboard_shortcut_manager.register_key_shortcut(
-            key_set=keyboard_shortcut_manager.create_key_set(
-                key="Arrow Right", shift=True, ctrl=False, alt=True, meta=False
-            ),
-            func=self.on_click_next_page,
-        )
-        # 操作履歴一覧のページ送り / 前のページへ
-        keyboard_shortcut_manager.register_key_shortcut(
-            key_set=keyboard_shortcut_manager.create_key_set(
-                key="Arrow Left", shift=True, ctrl=False, alt=True, meta=False
-            ),
-            func=self.on_click_previous_page,
+            func=lambda e: self.tabRequest.focus()
         )
 
     @Logging.func_logger
@@ -143,89 +104,9 @@ class DashboardForm(ft.Card):
                     key="F", shift=True, ctrl=False, alt=True, meta=False
                 ),
             )
-            # 概要に含まれる文字の検索を実行
-            keyboard_shortcut_manager.unregister_key_shortcut(
-                key_set=keyboard_shortcut_manager.create_key_set(
-                    key="Enter", shift=False, ctrl=True, alt=False, meta=False
-                ),
-            )
-            # 操作履歴一覧の再読み込み
-            keyboard_shortcut_manager.unregister_key_shortcut(
-                key_set=keyboard_shortcut_manager.create_key_set(
-                    key="R", shift=False, ctrl=True, alt=True, meta=False
-                ),
-            )
-            # 操作履歴一覧のページ送り / 次のページへ
-            keyboard_shortcut_manager.unregister_key_shortcut(
-                key_set=keyboard_shortcut_manager.create_key_set(
-                    key="Arrow Right", shift=True, ctrl=False, alt=True, meta=False
-                ),
-            )
-            # 操作履歴一覧のページ送り / 前のページへ
-            keyboard_shortcut_manager.unregister_key_shortcut(
-                key_set=keyboard_shortcut_manager.create_key_set(
-                    key="Arrow Left", shift=True, ctrl=False, alt=True, meta=False
-                ),
-            )
 
     def refresh(self, e=None):
-        # ActivityRowHelper.refresh_data_rows(self)
-        # ActivityRowHelper.refresh_page_indicator(self)
-        pass
-
-    @Logging.func_logger
-    def on_request_edit_open(self, e):
-        if SessionHelper.logout_if_session_expired(self.page, self.session): return
-        self.session.set("request_id", e.control.content.value)
-        # TODO: アクティビティの詳細をダイアログで表示する場合は別途実装する
-        # self.open_edit_request_dialog()
-
-    @Logging.func_logger
-    def on_change_filter_activity_user(self, e):
-        if SessionHelper.logout_if_session_expired(self.page, self.session): return
-        self.session.set("filter_activity_user", e.control.value)
-        ActivityRowHelper.query_activity_all(self)
-        self.refresh()
-
-    @Logging.func_logger
-    def on_change_filter_activity_type(self, e):
-        if SessionHelper.logout_if_session_expired(self.page, self.session): return
-        self.session.set("filter_activity_type", e.control.value)
-        ActivityRowHelper.query_activity_all(self)
-        self.refresh()
-
-    @Logging.func_logger
-    def on_click_heading_column(self, e):
-        if SessionHelper.logout_if_session_expired(self.page, self.session): return
-        ActivityRowHelper.sort_column(self, self.session, e.control.label.value)
-
-    @Logging.func_logger
-    def on_click_next_page(self, e):
-        if SessionHelper.logout_if_session_expired(self.page, self.session): return
-        if self.btnNextPage.disabled:
-            self._unlock_form_controls()
-            return
-        request_data_count = ActivityRowHelper.count_activity_all(self)
-        if (self.data_row_offset + 1) < request_data_count:
-            self.data_row_offset += self.DATA_ROW_MAX
-            self.refresh()
-
-    @Logging.func_logger
-    def on_click_previous_page(self, e):
-        if SessionHelper.logout_if_session_expired(self.page, self.session): return
-        if self.btnPreviousPage.disabled:
-            self._unlock_form_controls()
-            return
-        if (self.data_row_offset + 1) > self.DATA_ROW_MAX:
-            self.data_row_offset -= self.DATA_ROW_MAX
-            self.refresh()
-
-    @Logging.func_logger
-    def on_click_search_summary(self, e):
-        if SessionHelper.logout_if_session_expired(self.page, self.session): return
-        self.data_row_offset = 0
-        self.session.set("request_text_search_string", self.tfSearchSummary.value)
-        self.refresh()
+        self.formInProgress.refresh()
 
     @Logging.func_logger
     def _lock_form_controls(self):
